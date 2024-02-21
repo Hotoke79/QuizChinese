5d2ddc0790b05599d53be80292ff2aae9bffcb16
Hotoke79 <dalnegorsk79@gmail.com>
2024-02-22 00:43:19 +1000

    Prettify

--- idiom_mods.js
+++ idiom_mods.js
@@ -1,1 +1,131 @@
-const emergency={name:"Срочность, неотложность",descriptio:["hang on by one's eyelids</br>比喻情况万分危急</br>bǐ yù qíng kuàng wàn fēn wēi jí","no time to delay</br>指形势紧迫，一刻也不允许拖延</br>zhǐ xíng shì jǐn pò ， yī kè yě bù yǔn xǔ tuō yán","jumped at","posthaste</br> 形容事情紧急到了极点（多用于公文、电报等)</br>  xíng róng shì qíng jǐn jí dào liǎo jí diǎn (duō yòng yú gōng wén, diàn bào děng)","a desperate situation</br>火烧到眉毛。比喻事到眼前，非常急迫</br>huǒ shāo dào méi máo 。 bǐ yù shì dào yǎn qián, fēi cháng jí pò","an extremely urgency</br>"],reveal:["千钧一发</br>qiān jūn yī fà","刻不容缓</br>kè bù róng huǎn","迫不及待</br>pò bù jí dài","十万火急</br>shí wàn huǒ jí","火烧眉毛</br>huǒ shāo méi máo","燃眉之急</br> rán méi zhī jí"]},insects={name:"Насекомые",descriptio:["seeking one's own doom</br>","disappearance act from an entangled situation</br>蝉变为成虫时要脱去一层壳。比喻用计脱身，使人不能及时发觉</br>chán biàn wéi chéng chóng shí yào tuō qù yī céng ké 。 bǐ yù yòng jì tuō shēn ， shǐ rén bù néng jí shí fā jué","succeed in the imperial examination</br>","nibble and gobble</br>比喻用各种方式侵占吞并</br>bǐ yù yòng gè zhǒng fāng shì qīn zhān tūn bìng","指蜻蜓在水面飞行时用尾部轻触水面的动作。比喻做事肤浅不深入</br>zhǐ qīng tíng zài shuǐ miàn fēi xíng shí yòng wěi bù qīng chù shuǐ miàn de dòng zuò 。 bǐ yù zuò shì fū qiǎn bù shēn rù","比喻自不量力，招致失败。同“螳臂当车</br>bǐ yù zì bù liáng lì ， zhāo zhì shī bài 。 tóng táng bì dāng chē","比喻事情所留下的隐约可寻的痕迹和线索</br>bǐ yù shì qíng suǒ liú xià de yǐn yuē kě xún de hén jì hé xiàn suǒ","covet gains ahead without being aware of danger behind as the mantis stalk the cicade,unaware of the oriole behind"],reveal:["飞蛾扑火</br>fēi é pū huǒ","金蝉脱壳</br>jīn chán tuō qiào","蟾宫折桂</br>chán gōng zhé guì","蚕食鲸吞</br>cán shí jīng tūn","蜻蜓点水</br>qīng tíng diǎn shuǐ","螳臂挡车</br>táng bì dǎng chē","蛛丝马迹</br>zhū sī mǎ jì","螳螂捕蝉，黄雀在后</br>táng láng bǔ chán，huáng què zài hòu"]},animals={name:"Животные",descriptio:["all things take on a new aspect</br> 万象：宇宙间一切景象；更：变更。事物或景象改换了样子，出现了一番新气象</br>  wàn xiàng ： yǔ zhòu jiān yī qiē jǐng xiàng ； gēng ： biàn gēng 。 shì wù huò jǐng xiàng gǎi huàn liǎo yàng zǐ ， chū xiàn liǎo yī fān xīn qì xiàng","flee helter-skelter</br>抱着头，象老鼠那样惊慌逃跑。形容受到打击后狼狈逃跑</br>bào zhuó tóu ， xiàng lǎo shǔ nà yàng jīng huāng táo pǎo 。 xíng róng shòu dào dǎ jī hòu láng bèi táo pǎo","small tricks</br>指微不足道的本领。也指偷偷摸摸的行为</br>zhǐ wēi bù zú dào de běn lǐng 。 yě zhǐ tōu tōu mō mō de xíng wéi","a large number of mounted and foot soldiers</br> 形容雄壮的队伍或浩大的声势</br>xíng róng xióng zhuàng de duì wǔ huò hào dà de shēng shì","lock the stable door after the horse has bolted</br>羊逃跑了再去修补羊圈，还不算晚。比喻出了问题以后想办法补救，可以防止继续受损失</br>yáng táo pǎo liǎo zài qù xiū bǔ yáng quān ， huán bù suàn wǎn 。 bǐ yù chū liǎo wèn tí yǐ hòu xiǎng bàn fǎ bǔ jiù ， kě yǐ fáng zhǐ jì xù shòu sǔn shī","extremely suspicious</br>将映在酒杯里的弓影误认为蛇。比喻因疑神疑鬼而引起恐惧</br>jiāng yìng zài jiǔ bēi lǐ de gōng yǐng wù rèn wéi shé 。 bǐ yù yīn yí shén yí guǐ ér yǐn qǐ kǒng jù","distinguished, outstanding man in a common crowd; stand head and shoulders above</br>比喻一个人的仪表或才能在周围一群人里显得很突出</br>bǐ yù yī gè rén de yí biǎo huò cái néng zài zhōu wéi yī qún rén lǐ xiǎn dé hěn tū chū","cast pearls before swine</br> 讥笑听话的人不懂对方说的是什么。也用以讥笑说话的人不看对象</br>jī xiào tīng huà de rén bù dǒng duì fāng shuō de shì shén me 。 yě yòng yǐ jī xiào shuō huà de rén bù kàn duì xiàng","feel just like fish in water</br>比喻有所凭借。也比喻得到跟自己十分投合的人或对自己很合适的环境</br>bǐ yù yǒu suǒ píng jiè 。 yě bǐ yù dé dào gēn zì jǐ shí fēn tóu hé de rén huò duì zì jǐ hěn hé shì de huán jìng","birds sing and flowers give forth their fragrance </br>鸟叫得好听，花开得喷香。形容春天的美好景象</br>niǎo jiào dé hǎo tīng ， huā kāi dé pēn xiāng 。 xíng róng chūn tiān de měi hǎo jǐng xiàng","hold a candle to the devil </br>  伥：伥鬼，古时传说被老虎吃掉的人，死后变成伥鬼，专门引诱人来给老虎吃。替老虎做伥鬼。比喻充当恶人的帮凶","All tricks have been exhausted </br> 比喻有限的一点本领也已经用完了</br>bǐ yù yǒu xiàn de yī diǎn běn lǐng yě yǐ jīng yòng wán liǎo","shortsighted</br>老鼠的眼睛只能看到一米远。形容目光短浅，没有远见</br>lǎo shǔ de yǎn jīng zhī néng kàn dào yī mǐ yuǎn 。 xíng róng mù guāng duǎn qiǎn ， méi yǒu yuǎn jiàn","a person of a stocky and imposing build</br>背宽厚如虎，腰粗壮如熊。形容人身体魁梧健壮</br>bèi kuān hòu rú hǔ ， yāo cū zhuàng rú xióng 。 xíng róng rén shēn tǐ kuí wú jiàn zhuàng","stand by a stump waiting for more hares to come and clash themselves against it--trust to chance and windfalls</br>原比喻希图不经过努力而得到成功的侥幸心理。现也比喻死守狭隘经验，不知变通</br>bǐ yù xī tú bù jīng guò nǔ lì ér dé dào chéng gōng de jiǎo xìng xīn lǐ 。 xiàn yě bǐ yù sǐ shǒu xiá ài jīng yàn ， bù zhī biàn tōng","white hair and ruddy complexion hale and hearty</br>仙鹤羽毛般雪白的头发，儿童般红润的面色。形容老年人气色好</br>xiān hè yǔ máo bān xuě bái de tóu fā ， ér tóng bān hóng rùn de miàn sè 。 xíng róng lǎo nián rén qì sè hǎo"],reveal:["万象更新</br>wàn xiàng gēng xīn","抱头鼠窜</br>bào tóu shǔ cuàn","鸡鸣狗盗</br>jī míng gǒu dào","千军万马</br>qiān jūn wàn mǎ","亡羊补牢</br>wáng yáng bǔ láo","杯弓蛇影</br>bēi gōng shé yǐng","鹤立鸡群</br>hè lì jī qún","对牛弹琴</br>duì niú tán qín","如鱼得水</br>rú yú dé shuǐ","鸟语花香</br>niǎo yǔ huā xiāng","为虎作伥 </br>wèi hǔ zuò chāng","黔驴技穷</br>qián lǘ jì qióng","鼠目寸光</br>shǔ mù cùn guāng","虎背熊腰</br>hǔ bèi xióng yāo","守株待兔</br>shǒu zhū dài tù","鹤发童颜</br>hè fà tóng yán"]},analogues={name:"Аналогии",descriptio:["Дословно: хороший меч не стареет. А если по-русски то: старый конь борозды не испортит или тряхнуть стариной","Дословно: один камень - две птицы. По-нашему будет конечно же - одним выстрелом убить двух зайцев.","Дословно: утопить лодку и разбить котел. Т.е. сжечь мосты, исключить пути к отступлению.","Дословно: днем видеть сны. Русские аналоги: витать в облаках, строить замки из песка. Т.е. предаваться малосбыточным мечтам.","Дословно: любишь дом - люби и ворона (на крыше). любишь кататься - люби и санки возить.","Дословно: материала много, а пользы мало. Велика фигура, да дура по-русски.","Дословно: нарисовав дракона подрисовать зрачки. Иначе: последний штрих, вишенка на торте.","Дословно: камень утонул в море. По-нашему: кануть в воду, бесследно исчезнуть.","Дословно: слева, справа - становится трудно. Куда ни киннь - везде клин","Дословно: зуб менять на зуб. У нас - глаз за глаз","Дословно: Мутная вода, нащупывать рыбу. По-нашему: ловить рыбу в мутной воде","Дословно: На половину войти в землю. По-нашему: одной ногой в могиле, дышать на ладан.","Дословно: начало, конец - сног на голову. По-нашему: ставить телегу впереди лошади","Дословно: беда с неба падает. Как снег на голову","Голова тяжелая, а ноги леггие. По-нашему: колосс на глинянных ногах</b>. Говориться о чем то массивном, но не устойчивом.","Дословно: Плакать, смеяться не получается. По-нашему: смех сквозь слезы."],reveal:["宝刀不老 </br> bǎo dāo bù lǎo","一石二鸟 </br> yī shí èr niǎo","沉舟破釜 </br> chén zhōu pò fǔ","白日做梦 </br> bái rì zuò mèng","爱屋及乌 </br> ài wū jí wū","大材小用 </br> dà cái xiǎo yòng","画龙点睛 </br> huà lóng diǎn jīng","石沉大海 </br> shí chén dà hǎi","左右为难 </br> zuǒ yòu wéi nán","以牙还牙 </br> yǐ yá huán yá","浑水摸鱼 </br> hún shuǐ mō yú","半截入土 </br> bàn jié rù tǔ","本末倒置 </br> běn mò dǎo zhì","祸从天降 </br> huò cóng tiān jiàng","头重脚轻 </br> tóu zhòng jiǎo qīng","哭笑不得 </br> kū xiào bù dé"]},roundsJs=[analogues,animals,insects,emergency];
+const emergency = {
+    "name": "Срочность, неотложность",
+    "descriptio": [
+      "hang on by one's eyelids</br>比喻情况万分危急</br>bǐ yù qíng kuàng wàn fēn wēi jí",
+      "no time to delay</br>指形势紧迫，一刻也不允许拖延</br>zhǐ xíng shì jǐn pò ， yī kè yě bù yǔn xǔ tuō yán</br>brook no delay; demand immediate attention; be of great urgency",
+      "jumped at",
+      "posthaste</br> 形容事情紧急到了极点（多用于公文、电报等)</br>  xíng róng shì qíng jǐn jí dào liǎo jí diǎn (duō yòng yú gōng wén, diàn bào děng)",
+      "a desperate situation</br>火烧到眉毛。比喻事到眼前，非常急迫</br>huǒ shāo dào méi máo 。 bǐ yù shì dào yǎn qián, fēi cháng jí pò",
+      "an extremely urgency</br>"
+    ],
+    "reveal":[
+      "千钧一发</br>qiān jūn yī fà",
+      "刻不容缓</br>kè bù róng huǎn",
+      "迫不及待</br>pò bù jí dài",
+      "十万火急</br>shí wàn huǒ jí",
+      "火烧眉毛</br>huǒ shāo méi máo",
+      "燃眉之急</br> rán méi zhī jí"
+    ]
+  }
+  
+  const insects = {
+    "name":"Насекомые",
+    "descriptio":[
+      "seeking one's own doom</br>",
+      "disappearance act from an entangled situation</br>蝉变为成虫时要脱去一层壳。比喻用计脱身，使人不能及时发觉</br>chán biàn wéi chéng chóng shí yào tuō qù yī céng ké 。 bǐ yù yòng jì tuō shēn ， shǐ rén bù néng jí shí fā jué",
+      "succeed in the imperial examination</br>",
+      "nibble and gobble</br>比喻用各种方式侵占吞并</br>bǐ yù yòng gè zhǒng fāng shì qīn zhān tūn bìng",
+      "指蜻蜓在水面飞行时用尾部轻触水面的动作。比喻做事肤浅不深入</br>zhǐ qīng tíng zài shuǐ miàn fēi xíng shí yòng wěi bù qīng chù shuǐ miàn de dòng zuò 。 bǐ yù zuò shì fū qiǎn bù shēn rù",
+      "比喻自不量力，招致失败。同“螳臂当车</br>bǐ yù zì bù liáng lì ， zhāo zhì shī bài 。 tóng táng bì dāng chē",
+      "比喻事情所留下的隐约可寻的痕迹和线索</br>bǐ yù shì qíng suǒ liú xià de yǐn yuē kě xún de hén jì hé xiàn suǒ",
+      "covet gains ahead without being aware of danger behind as the mantis stalk the cicade,unaware of the oriole behind"
+    ],
+    "reveal":[
+      "飞蛾扑火</br>fēi é pū huǒ",
+      "金蝉脱壳</br>jīn chán tuō qiào",
+      "蟾宫折桂</br>chán gōng zhé guì",
+      "蚕食鲸吞</br>cán shí jīng tūn",
+      "蜻蜓点水</br>qīng tíng diǎn shuǐ",
+      "螳臂挡车</br>táng bì dǎng chē",
+      "蛛丝马迹</br>zhū sī mǎ jì",
+      "螳螂捕蝉，黄雀在后</br>táng láng bǔ chán，huáng què zài hòu"
+    ]
+  }
+  
+  const animals ={
+    "name": "Животные",
+    "descriptio": [
+      "all things take on a new aspect</br> 万象：宇宙间一切景象；更：变更。事物或景象改换了样子，出现了一番新气象</br>  wàn xiàng ： yǔ zhòu jiān yī qiē jǐng xiàng ； gēng ： biàn gēng 。 shì wù huò jǐng xiàng gǎi huàn liǎo yàng zǐ ， chū xiàn liǎo yī fān xīn qì xiàng",
+      "flee helter-skelter</br>抱着头，象老鼠那样惊慌逃跑。形容受到打击后狼狈逃跑</br>bào zhuó tóu ， xiàng lǎo shǔ nà yàng jīng huāng táo pǎo 。 xíng róng shòu dào dǎ jī hòu láng bèi táo pǎo",
+      "small tricks</br>指微不足道的本领。也指偷偷摸摸的行为</br>zhǐ wēi bù zú dào de běn lǐng 。 yě zhǐ tōu tōu mō mō de xíng wéi",
+      "a large number of mounted and foot soldiers</br> 形容雄壮的队伍或浩大的声势</br>xíng róng xióng zhuàng de duì wǔ huò hào dà de shēng shì",
+      "lock the stable door after the horse has bolted</br>羊逃跑了再去修补羊圈，还不算晚。比喻出了问题以后想办法补救，可以防止继续受损失</br>yáng táo pǎo liǎo zài qù xiū bǔ yáng quān ， huán bù suàn wǎn 。 bǐ yù chū liǎo wèn tí yǐ hòu xiǎng bàn fǎ bǔ jiù ， kě yǐ fáng zhǐ jì xù shòu sǔn shī",
+      "extremely suspicious</br>将映在酒杯里的弓影误认为蛇。比喻因疑神疑鬼而引起恐惧</br>jiāng yìng zài jiǔ bēi lǐ de gōng yǐng wù rèn wéi shé 。 bǐ yù yīn yí shén yí guǐ ér yǐn qǐ kǒng jù",
+      "distinguished, outstanding man in a common crowd; stand head and shoulders above</br>比喻一个人的仪表或才能在周围一群人里显得很突出</br>bǐ yù yī gè rén de yí biǎo huò cái néng zài zhōu wéi yī qún rén lǐ xiǎn dé hěn tū chū",
+      "cast pearls before swine</br> 讥笑听话的人不懂对方说的是什么。也用以讥笑说话的人不看对象</br>jī xiào tīng huà de rén bù dǒng duì fāng shuō de shì shén me 。 yě yòng yǐ jī xiào shuō huà de rén bù kàn duì xiàng",
+      "feel just like fish in water</br>比喻有所凭借。也比喻得到跟自己十分投合的人或对自己很合适的环境</br>bǐ yù yǒu suǒ píng jiè 。 yě bǐ yù dé dào gēn zì jǐ shí fēn tóu hé de rén huò duì zì jǐ hěn hé shì de huán jìng",
+      "birds sing and flowers give forth their fragrance </br>鸟叫得好听，花开得喷香。形容春天的美好景象</br>niǎo jiào dé hǎo tīng ， huā kāi dé pēn xiāng 。 xíng róng chūn tiān de měi hǎo jǐng xiàng",
+      "hold a candle to the devil </br>  伥：伥鬼，古时传说被老虎吃掉的人，死后变成伥鬼，专门引诱人来给老虎吃。替老虎做伥鬼。比喻充当恶人的帮凶",
+      "All tricks have been exhausted </br> 比喻有限的一点本领也已经用完了</br>bǐ yù yǒu xiàn de yī diǎn běn lǐng yě yǐ jīng yòng wán liǎo",
+      "shortsighted</br>老鼠的眼睛只能看到一米远。形容目光短浅，没有远见</br>lǎo shǔ de yǎn jīng zhī néng kàn dào yī mǐ yuǎn 。 xíng róng mù guāng duǎn qiǎn ， méi yǒu yuǎn jiàn",
+      "a person of a stocky and imposing build</br>背宽厚如虎，腰粗壮如熊。形容人身体魁梧健壮</br>bèi kuān hòu rú hǔ ， yāo cū zhuàng rú xióng 。 xíng róng rén shēn tǐ kuí wú jiàn zhuàng",
+      "stand by a stump waiting for more hares to come and clash themselves against it--trust to chance and windfalls</br>原比喻希图不经过努力而得到成功的侥幸心理。现也比喻死守狭隘经验，不知变通</br>bǐ yù xī tú bù jīng guò nǔ lì ér dé dào chéng gōng de jiǎo xìng xīn lǐ 。 xiàn yě bǐ yù sǐ shǒu xiá ài jīng yàn ， bù zhī biàn tōng",
+      "white hair and ruddy complexion hale and hearty</br>仙鹤羽毛般雪白的头发，儿童般红润的面色。形容老年人气色好</br>xiān hè yǔ máo bān xuě bái de tóu fā ， ér tóng bān hóng rùn de miàn sè 。 xíng róng lǎo nián rén qì sè hǎo"
+  
+  ],
+  "reveal": [
+    "万象更新</br>wàn xiàng gēng xīn",
+    "抱头鼠窜</br>bào tóu shǔ cuàn",
+    "鸡鸣狗盗</br>jī míng gǒu dào",
+    "千军万马</br>qiān jūn wàn mǎ",
+    "亡羊补牢</br>wáng yáng bǔ láo",
+    "杯弓蛇影</br>bēi gōng shé yǐng",
+    "鹤立鸡群</br>hè lì jī qún",
+    "对牛弹琴</br>duì niú tán qín",
+    "如鱼得水</br>rú yú dé shuǐ",
+    "鸟语花香</br>niǎo yǔ huā xiāng",
+    "为虎作伥 </br>wèi hǔ zuò chāng",
+    "黔驴技穷</br>qián lǘ jì qióng",
+    "鼠目寸光</br>shǔ mù cùn guāng",
+    "虎背熊腰</br>hǔ bèi xióng yāo",
+    "守株待兔</br>shǒu zhū dài tù",
+    "鹤发童颜</br>hè fà tóng yán",
+  ]
+  }
+  
+  const analogues =
+  {
+    "name": "Аналогии",
+    "descriptio": ["Дословно: хороший меч не стареет. А если по-русски то: старый конь борозды не испортит или тряхнуть стариной",
+      "Дословно: один камень - две птицы. По-нашему будет конечно же - одним выстрелом убить двух зайцев.",
+      "Дословно: утопить лодку и разбить котел. Т.е. сжечь мосты, исключить пути к отступлению.",
+      "Дословно: днем видеть сны. Русские аналоги: витать в облаках, строить замки из песка. Т.е. предаваться малосбыточным мечтам.",
+      "Дословно: любишь дом - люби и ворона (на крыше). любишь кататься - люби и санки возить.",
+      "Дословно: материала много, а пользы мало. Велика фигура, да дура по-русски.",
+      "Дословно: нарисовав дракона подрисовать зрачки. Иначе: последний штрих, вишенка на торте.",
+      "Дословно: камень утонул в море. По-нашему: кануть в воду, бесследно исчезнуть.",
+      "Дословно: слева, справа - становится трудно. Куда ни киннь - везде клин",
+      "Дословно: зуб менять на зуб. У нас - глаз за глаз",
+      "Дословно: Мутная вода, нащупывать рыбу. По-нашему: ловить рыбу в мутной воде",
+      "Дословно: На половину войти в землю. По-нашему: одной ногой в могиле, дышать на ладан.",
+      "Дословно: начало, конец - сног на голову. По-нашему: ставить телегу впереди лошади",
+      "Дословно: беда с неба падает. Как снег на голову",
+      "Голова тяжелая, а ноги леггие. По-нашему: колосс на глинянных ногах</b>. Говориться о чем то массивном, но не устойчивом.",
+      "Дословно: Плакать, смеяться не получается. По-нашему: смех сквозь слезы."
+    ],
+  
+    "reveal": [
+      "宝刀不老 </br> bǎo dāo bù lǎo",
+      "一石二鸟 </br> yī shí èr niǎo",
+      "沉舟破釜 </br> chén zhōu pò fǔ",
+      "白日做梦 </br> bái rì zuò mèng",
+      "爱屋及乌 </br> ài wū jí wū",
+      "大材小用 </br> dà cái xiǎo yòng",
+      "画龙点睛 </br> huà lóng diǎn jīng",
+      "石沉大海 </br> shí chén dà hǎi",
+      "左右为难 </br> zuǒ yòu wéi nán",
+      "以牙还牙 </br> yǐ yá huán yá",
+      "浑水摸鱼 </br> hún shuǐ mō yú",
+      "半截入土 </br> bàn jié rù tǔ",
+      "本末倒置 </br> běn mò dǎo zhì",
+      "祸从天降 </br> huò cóng tiān jiàng",
+      "头重脚轻 </br> tóu zhòng jiǎo qīng",
+      "哭笑不得 </br> kū xiào bù dé"]
+      
+  }
+  const roundsJs = [
+    analogues,
+    animals, 
+    insects,
+    emergency
+  ]
